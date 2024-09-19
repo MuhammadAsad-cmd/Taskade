ca19a3742a0ffc32311c1c6a4bb5d3c6fa630c0b
MuhammadAsad-cmd <ma6530028@gmail.com>
2024-09-19 22:21:36 +0500

    task first

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,21 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'dark-gray': '#17191C', 
+        'appcolor': 'rgb(37, 40, 45)', 
+        'vivid-pink':"rgb(255, 45, 96)",
+        'light-gray': '#eaebef',
+        'upgrade':"#f6c349",
+        'medium-gray': '#b5b6ba',
+        'dark-maroon': '#452029',
+        'primary':"#ff2d62",
+        "applight":"#18191d",
+        'slate-gray': '#1c1f22',
+        "textcolor":"#606467",
+        "cool-gray":"#93999F", 
+        'light-gray': '#e3e5e8',
+        'dark-charcoal': '#2c3035',
+        'slate-gray': '#484d51',
       },
     },
   },
