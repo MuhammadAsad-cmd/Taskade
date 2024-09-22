2033d8e93e4c6cfa55ec9a8611f20a16725249db
MuhammadAsad-cmd <ma6530028@gmail.com>
2024-09-22 20:48:14 +0500

    jj

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
               {children}
             </div>
           </div>
-          <RightSidebar />
+          {/* <RightSidebar /> */}
         </div>
       </body>
     </html>
